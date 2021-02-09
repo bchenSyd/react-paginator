1c6ff6b64a5307dde08ec10081905369a40b2a93
Bo Chen <bochen2014@yahoo.com>
2021-02-09 17:13:35 +1100

    implementation

--- src/paginator.jsx
+++ src/paginator.jsx
@@ -1,11 +1,70 @@
 import React from "react";
+import styled from "styled-components";
 
-const Paginator = ({ para }) => {
+const StyledWrapper = styled.div`
+  span,
+  a {
+    &:not(:first-child) {
+      margin-left: 20px;
+    }
+  }
+`;
+
+const Paginator = ({
+  totalPageNumber,
+  currentPageNumber,
+  onPageNavigation,
+}) => {
+  const pageNumbers = Array.from({ length: 5 })
+    .map((_, i) => currentPageNumber - 2 + i)
+    .filter((n) => n > 0 && n <= totalPageNumber);
+
+  const showLeftLump = () => {
+    const leftLump = [];
+    if (currentPageNumber > 3) {
+      leftLump.push(
+        <a href="#" key="page_1" onClick={onPageNavigation(1)}>
+          1
+        </a>
+      );
+    }
+    if (currentPageNumber > 4) {
+      leftLump.push(<span key="page_left_lump">...</span>);
+    }
+    return leftLump;
+  };
+  const showRightLump = () => {
+    const rightLump = [];
+    if (totalPageNumber > currentPageNumber + 3) {
+      rightLump.push(<span key="page_right_lump">...</span>);
+    }
+    if (totalPageNumber > currentPageNumber + 2) {
+      rightLump.push(
+        <a
+          href="#"
+          key={`page_${totalPageNumber}`}
+          onClick={onPageNavigation(totalPageNumber)}
+        >
+          {totalPageNumber}
+        </a>
+      );
+    }
+    return rightLump;
+  };
   return (
-    <>
-      <div>something</div>
-      {para && <div>{para}</div>}
-    </>
+    <StyledWrapper>
+      {showLeftLump()}
+      {pageNumbers.map((p) =>
+        p === currentPageNumber ? (
+          <span key={`page_${p}`}>{p}</span>
+        ) : (
+          <a href="#" key={`page_${p}`} onClick={onPageNavigation(p)}>
+            {p}
+          </a>
+        )
+      )}
+      {showRightLump()}
+    </StyledWrapper>
   );
 };
 
--- src/paginator.stories.jsx
+++ src/paginator.stories.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Paginator from "./paginator";
 
 export default { component: Paginator, title: "Examples / Paginator" };
 
-export const Basic = () => <Paginator />;
+export const Basic = () => {
+  const totalPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  return (
+    <>
+      <h2>show content for page {currentPage}</h2>
+      <Paginator
+        totalPageNumber={totalPage}
+        currentPageNumber={currentPage}
+        onPageNavigation={(page) => () => {
+          setCurrentPage(page);
+        }}
+      />
+    </>
+  );
+};
 export const WithArgs = (args) => <Paginator {...args} />;
-WithArgs.args = { para: "Some parameters" };
-
+WithArgs.args = {
+  totalPageNumber: 1,
+  currentPageNumber: 1,
+  onPageNavigation: () => {},
+};
